cb7a8b247122ba3dd396751e54a658d52f56ea4e
Hack Reactor Students <gsenol@gmail.com>
2014-03-29 14:25:02 -0700

    can navigate between views now, and validate url on client side.

--- public/angular/main.js
+++ public/angular/main.js
@@ -8,14 +8,18 @@ angular.module('ShortlyApp', ['ngRoute'])
       templateUrl: 'angular/createLink.html',
       controller: 'ShortenController'
     })
+    .when('/', {
+      templateUrl: 'angular/allLinks.html',
+      controller: 'MainController'
+    })
     .otherwise({
-      template: '<h3>otherwise</h3>',
-      controller: 'ShortenController'
+      redirectTo: '/'
     });
 }])
 
 // main controller for getting links
 .controller('MainController', function($scope, $http){
+  console.log("i'm created!");
   $scope.person = {
     name: 'test'
   };
@@ -30,21 +34,35 @@ angular.module('ShortlyApp', ['ngRoute'])
       $scope.links = data.data;
     }); 
   };
-  // $scope.getLinks();
+  $scope.getLinks();
 })
 
 // controller for shortening and posting links
-.controller('ShortenController', function($scope, $http, $routeParams) {
+.controller('ShortenController', function($scope, $http) {
+  $scope.validUrl = true;
+
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
+  var isValidUrl = function(url) {
+    return url.match(rValidUrl);
+  };
+
   $scope.shortenLink = function () {
-    var promise = $http({
-      method: 'POST',
-      url: '/links',
-      // wrap in obj? {url: urlLink}
-      data: JSON.stringify({url: $scope.urlLink})
-    });
-    promise.then(function(link){
-      console.log('inside shorten promise: ', link);
-      // $scope.links = link.data;
-    }); 
+    if (isValidUrl($scope.urlLink)) {
+      $scope.validUrl = true;
+      var promise = $http({
+        method: 'POST',
+        url: '/links',
+        // wrap in obj? {url: urlLink}
+        data: JSON.stringify({url: $scope.urlLink})
+      });
+      promise.then(function(link){
+        console.log('inside shorten promise: ', link);
+        $scope.newLink = link.data;
+      });
+
+    } else {
+      $scope.validUrl = false;
+    } 
   };
 });
